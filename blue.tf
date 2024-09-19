5a689fe88b4ed62124e3e7fa9e8997ed4ccb0800
Hemed Gur-Ary <hemed.gur-ary@datameshgroup.com>
2024-09-19 14:06:25 +1000

    .

--- blue.tf
+++ blue.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "blue" {
   count = var.enable_blue_env ? var.blue_instance_count : 0
 
   ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = module.vpc.private_subnets[count.index % length(module.vpc.private_subnets)]
   vpc_security_group_ids = [module.app_security_group.security_group_id]
   user_data = templatefile("${path.module}/init-script.sh", {
