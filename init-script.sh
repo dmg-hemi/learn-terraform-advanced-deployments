04ab9957efd798b964770ccef6a35b98cf5bb52e
Hemed Gur-Ary <hemed.gur-ary@datameshgroup.com>
2024-09-19 14:55:50 +1000

    .

--- init-script.sh
+++ init-script.sh
@@ -2,7 +2,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-sudo dnf update -y
+sudo dnf upgrade -y
 sudo dnf install httpd -y
 sudo systemctl enable httpd
 sudo systemctl start httpd
--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-2023.*-kernel-6.1-x86_64"]
   }
 }
 
